488e98ad1baa6eb98e95c4c8641c6a551edf0e3d
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-10-22 01:00:02 -0500

    Contact buttosn

--- src/app/components/contact-buttons/contact-buttons.component.ts
+++ src/app/components/contact-buttons/contact-buttons.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactButtonsComponent implements OnInit {
 
+  contacts = [
+    {
+      contactMessage: "Write me a mail",
+      icon: "/assets/icons/mail.svg",
+      url: "mailto:viktorb.132@gmail.com",
+    },
+    {
+      contactMessage: "Linkedin",
+      icon: "/assets/icons/linkedin.svg",
+      url: "https://www.linkedin.com/in/viktor-bolanos-solis/",
+    },
+    {
+      contactMessage: "Github",
+      icon: "/assets/icons/github.svg",
+      url: "https://github.com/Vicktorbs",
+    },
+    {
+      contactMessage: "Twiter",
+      icon: "/assets/icons/twitter.svg",
+      url: "https://twitter.com/Viktor_b_Sq",
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
